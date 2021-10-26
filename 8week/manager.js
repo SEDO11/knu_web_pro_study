6d4117baa21249e4b37c4367bf2422f2237d9e37
SEDO11 <do11anm@naver.com>
2021-10-26 11:12:50 +0900

    211026 8주차 과제 완료

--- 8week/manager.js
+++ 8week/manager.js
@@ -1,10 +1,15 @@
 window.onload = function () {
-    var worker = new Worker("worker.js");
+        var worker = new Worker("worker.js");
 
-    worker.postMessage("ping");
-    worker.onmessage = function(event) {
-        var msg = "Worker says" + event.data;
-        alert(msg);
-        document.getElementById("output").innerHTML = msg;
-    }
+        worker.postMessage("ping");
+        worker.onmessage = function(event) {
+            var msg = "Worker says" + event.data;
+            alert(msg);
+            document.getElementById("output").innerHTML = msg;
+        }
+    
+    // This is in case of normal worker start
+    // "window" is not defined in web worker
+    // so if you load this file directly using `new Worker`
+    // the worker code will still execute properly
 }
